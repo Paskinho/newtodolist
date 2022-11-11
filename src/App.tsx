37b5e2ecc14502c44978d443566558cc0948d02e
Paskinho <v.paskanov@gmail.com>
2022-11-11 18:48:54 +0300

    DOP Tuesday (1 lesson Tuesday Main)

--- src/App.tsx
+++ src/App.tsx
@@ -10,23 +10,20 @@ export type TaskType = {
 }
 
 export type FilterValuesType = "all" | "active" | "completed"
-
+// Create +
+// Read => +, filtration
+// Update +
+// Delete +
 
 type TodolistType = {
     id: string
     title: string
-
-}
-
-type TasksStateType = {
-    [todoListId: string]: InTasksType
-}
-
-type InTasksType= {
-    data: Array <TaskType>
     filter: FilterValuesType
 }
 
+type TasksStateType ={
+    [todoListId: string]: Array<TaskType>
+}
 
 
 
@@ -38,42 +35,34 @@ function App() {
     const todoListId_2 = v1()
 
     const [todoLists, setTodoLists] = useState<Array<TodolistType>>([
-        {id: todoListId_1, title: "What to learn"},
-        {id: todoListId_2, title: "What to buy"}
+        {id: todoListId_1, title: "What to learn", filter: "all"},
+        {id: todoListId_2, title: "What to buy", filter: "all"}
     ])
     const [tasks, setTasks] = useState<TasksStateType>({
-        [todoListId_1]: {
-            data: [
-                {id: v1(), title: "HTML & CSS", isDone: true},
-                {id: v1(), title: "JS & ES6", isDone: true},
-                {id: v1(), title: "REACT & TS", isDone: false},
-                ],
-            filter: "all"
-        },
-
-    [todoListId_2]: {
-    data: [
-        {id: v1(), title: "COLA", isDone: true},
-        {id: v1(), title: "MILK", isDone: true},
-        {id: v1(), title: "WATER", isDone: false},
-    ],
-        filter: "all"
-
-}
-})
+        [todoListId_1]: [
+            {id: v1(), title: "HTML & CSS", isDone: true},
+            {id: v1(), title: "JS & ES6", isDone: true},
+            {id: v1(), title: "REACT & TS", isDone: false},
+        ],
+        [todoListId_2]: [
+            {id: v1(), title: "COLA", isDone: true},
+            {id: v1(), title: "MILK", isDone: true},
+            {id: v1(), title: "WATER", isDone: false},
+        ]
+    })
 
     //
     // const todoListTitle: string = "What to learn"
 
     const removeTask = (taskId: string, todoListId: string) => {
-        const tasksForUpdate: Array<TaskType> = tasks[todoListId]
-        const resultOfUpdate: Array<TaskType> = tasksForUpdate.filter(t => t.id !== taskId)
-        const copyTasks = {...tasks}
-        tasks[todoListId] = resultOfUpdate
+        const tasksForUpdate: Array<TaskType>=tasks[todoListId]
+        const resultOfUpdate: Array<TaskType> = tasksForUpdate.filter(t=> t.id !== taskId)
+        const copyTasks ={...tasks}
+        tasks[todoListId]=resultOfUpdate
         setTasks(copyTasks)
 
         // короткая запись:
-        setTasks({...tasks, [todoListId]: tasks[todoListId].filter(t => t.id !== taskId)})
+        setTasks({...tasks,[todoListId]: tasks[todoListId].filter(t=> t.id !== taskId)})
 
     }
     const addTask = (title: string, todoListId: string) => {
@@ -83,21 +72,18 @@ function App() {
             title, // title: title
             isDone: false
         }
-        const resultOfUpdate: Array<TaskType> = [...tasksForUpdate, newTask]
-        const copyTasks: TasksStateType = {...tasks}
+        const resultOfUpdate: Array<TaskType> = [...tasksForUpdate,newTask]
+        const copyTasks: TasksStateType={...tasks}
         copyTasks[todoListId] = resultOfUpdate
         setTasks(copyTasks)
 
         //короткая запись
-        setTasks({...tasks, [todoListId]: [...tasks[todoListId], newTask]})
+      setTasks({...tasks,[todoListId]:[...tasks[todoListId], newTask]})
     }
     const changeTaskStatus = (taskId: string, isDone: boolean, todoListId: string) => {
         const tasksForUpdate: Array<TaskType> = tasks[todoListId]
-        const resultOfUpdate: Array<TaskType> = tasksForUpdate.map(t => t.id === todoListId ? {
-            ...t,
-            isDone: isDone
-        } : t)
-        const copyTasks: TasksStateType = {...tasks}
+        const resultOfUpdate: Array<TaskType> = tasksForUpdate.map(t => t.id === todoListId ? {...t, isDone: isDone}: t)
+    const copyTasks: TasksStateType={...tasks}
         copyTasks[todoListId] = resultOfUpdate
         setTasks(copyTasks)
 
@@ -105,7 +91,7 @@ function App() {
 
 
     const changeTodoListFilter = (filter: FilterValuesType, todoListId: string) => {
-        setTodoLists(todoLists.map(tl => tl.id === todoListId ? {...tl, filter: filter} : tl))
+        setTodoLists(todoLists.map(tl => tl.id === todoListId ? {...tl, filter: filter}: tl))
     }
 
     const removeTodolist = (todoListId: string) => {
@@ -127,26 +113,27 @@ function App() {
     }
 
 
+
     const todoListComponents = todoLists.length
         ? todoLists.map(tl => {
-                const filteredTasks = getFilteredTasks(tasks[tl.id], tl.filter)
-                return (
-                    <TodoList
-                        key={tl.id}
-                        todoListId={tl.id}
-                        title={tl.title}
-                        tasks={filteredTasks}
-                        filter={tl.filter}
-                        addTask={addTask}
-                        removeTask={removeTask}
-                        removeTodolist={removeTodolist}
-                        changeTodoListFilter={changeTodoListFilter}
-                        changeTaskStatus={changeTaskStatus}
-                    />
-                )
-            }
-        )
-        : <span>Create your first TodoList!!!</span>
+        const filteredTasks=getFilteredTasks(tasks[tl.id],tl.filter)
+            return (
+                <TodoList
+                    key={tl.id}
+                    todoListId={tl.id}
+                    title={tl.title}
+                    tasks={filteredTasks}
+                    filter={tl.filter}
+                    addTask={addTask}
+                    removeTask={removeTask}
+                    removeTodolist={removeTodolist}
+                    changeTodoListFilter={changeTodoListFilter}
+                    changeTaskStatus={changeTaskStatus}
+                />
+            )
+        }
+    )
+: <span>Create your first TodoList!!!</span>
 
 
     //GUI:
