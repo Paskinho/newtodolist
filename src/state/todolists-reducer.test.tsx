f6fb0c4c43413de59fee09df12dd68190d9ccc0e
Paskinho <v.paskanov@gmail.com>
2022-12-17 14:23:56 +0300

    10 lesson add beforeEach

--- src/state/todolists-reducer.test.tsx
+++ src/state/todolists-reducer.test.tsx
@@ -9,13 +9,23 @@ import {
 import {v1} from 'uuid';
 import {FilterValuesType, TodolistType} from '../App';
 
-let todolistId1 = v1();
-let todolistId2 = v1();
 
-const startState: Array<TodolistType> = [
-    {id: todolistId1, title: "What to learn", filter: "all"},
-    {id: todolistId2, title: "What to buy", filter: "all"}
-]
+let todolistId1: string;
+let todolistId2: string;
+let startState: Array<TodolistType>
+
+
+beforeEach(()=> {
+    todolistId1 = v1();
+    todolistId2 = v1();
+
+     startState = [
+        {id: todolistId1, title: "What to learn", filter: "all"},
+        {id: todolistId2, title: "What to buy", filter: "all"}
+    ]
+})
+
+
 
 
 test('correct todolist should be removed', () => {
