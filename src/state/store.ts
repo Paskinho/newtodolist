17ad44f810e803293274badb0a5024ca0ca26f73
Paskinho <v.paskanov@gmail.com>
2023-01-16 19:27:09 +0300

    1 dop wednesday add composeEnhancers

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,15 +1,25 @@
 import { tasksReducer } from './tasks-reducer'
 import { todolistsReducer } from './todolists-reducer'
-import {combineReducers, legacy_createStore} from 'redux'
+import {combineReducers, compose, legacy_createStore} from 'redux'
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
+
+declare global {
+    interface Window {
+        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+    }
+}
+
 const rootReducer = combineReducers({
     tasks: tasksReducer,
     todolists: todolistsReducer
 })
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 // непосредственно создаём store
-export const store = legacy_createStore(rootReducer)
+export const store = legacy_createStore(rootReducer, composeEnhancers())
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
