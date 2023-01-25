2e495ccd0349e93de8f25fb19401a1d191edc438
Paskinho <v.paskanov@gmail.com>
2023-01-25 19:00:27 +0300

    12 lesson. Add AddItemForm.stories.tsx

---
+++ src/stories/AddItemForm.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Button } from './Button';
+import {AddItemForm} from "../AddItemForm";
+import {action} from "@storybook/addon-actions";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'TODOLIST/AddItemForm',
+  component: AddItemForm,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    addItem : {
+      description: "Button clicked inside form"
+    }
+  },
+} as ComponentMeta<typeof AddItemForm>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof AddItemForm> = (args) => <AddItemForm {...args} />;
+
+export const AddItemFormStory = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+AddItemFormStory.args = {
+ addItem: action('Button clicked inside form')
+};
