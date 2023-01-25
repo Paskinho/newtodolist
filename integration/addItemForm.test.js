4acc2f43f29b823dcec5f1b2054142f50365f60d
Paskinho <v.paskanov@gmail.com>
2023-01-25 21:40:12 +0300

    12 lesson. testing with snapshot

---
+++ integration/addItemForm.test.js
@@ -0,0 +1,11 @@
+
+describe('addItemForm', () => {
+    it('base example, visually looks correct', async () => {
+        // APIs from jest-puppeteer
+        await page.goto('http://localhost:6006/iframe.html?args=&id=todolist-additemform--add-item-form-story&viewMode=story')
+        const image = await page.screenshot()
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot()
+    })
+})
