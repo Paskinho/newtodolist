843fd965e324a7e367b890ca415f0d0688bc68cd
Paskinho <v.paskanov@gmail.com>
2023-02-07 17:03:22 +0300

    13 lesson start.

--- src/index.tsx
+++ src/index.tsx
@@ -15,8 +15,8 @@ root.render(
     <App />
     </Provider>
 );
-
+reportWebVitals(console.log))
+// or send to an analytics endp
 // If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// to log results (for example:oint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
