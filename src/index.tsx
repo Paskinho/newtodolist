45946020bfb3baabb5ed24c4a19970f1fd008c48
Paskinho <v.paskanov@gmail.com>
2023-02-12 15:19:19 +0300

    13 lesson. Add axios

--- src/index.tsx
+++ src/index.tsx
@@ -16,7 +16,7 @@ root.render(
     <App />
     </Provider>
 );
-reportWebVitals(console.log))
+reportWebVitals(console.log)
 // or send to an analytics endp
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example:oint. Learn more: https://bit.ly/CRA-vitals
