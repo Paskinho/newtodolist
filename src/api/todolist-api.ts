9a5b230611038e1d805baf07ed381e1af9c5fba9
Paskinho <v.paskanov@gmail.com>
2023-02-22 18:53:06 +0300

    13 lesson. refactoring instance

--- src/api/todolist-api.ts
+++ src/api/todolist-api.ts
@@ -12,25 +12,25 @@ const instance = axios.create({
     withCredentials: true,
     headers: {
         // Не забываем заменить API-KEY на собственный
-        'API-KEY': '794181ab-6d62-4cfb-bc9f-d539dfac55f1',
+        'API-KEY': 'aeefc5de-7fbe-47c0-93be-ce3a4d1e3ad8',
     },
 })
 
 
 export const todolistApi = {
     updateTodolist(id: string, title: string) {
-        return axios.put(`https://social-network.samuraijs.com/api/1.1/todo-lists${id}`,{title},
+        return instance.put(`todo-lists${id}`,{title},
             settings)
 
     },
     getTodolists (){
-        return axios.get ('https://social-network.samuraijs.com/api/1.1/todo-lists', settings)
+        return instance.get ('todo-lists')
     },
     createTodolist(title: string){
-        return  axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists',{title},
+        return  instance.post('todo-lists',{title},
             settings)
     },
     deleteTodolist(todoId: string){
-        return axios.delete(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todoId}`, settings)
+        return instance.delete(`todo-lists/${todoId}`)
     }
 }
