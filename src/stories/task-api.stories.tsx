b323ffd3787afbe7469687ec4474385dd2e1c438
Paskinho <v.paskanov@gmail.com>
2023-02-25 15:10:05 +0300

    homework after 13 lesson. add CreateTask in task-api.stories.tsx

--- src/stories/task-api.stories.tsx
+++ src/stories/task-api.stories.tsx
@@ -1,2 +1,24 @@
+import React, {useEffect, useState} from "react";
+import {taskApi} from "../api/task-api";
+
+
+export default {
+    title: 'API'
+}
+
+
+export const CreateTask = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        const title = 'TypeScript';
+            const todolistId = 'aec989cc-ad3f-4a03-b961-43daea0380ce'
+        taskApi.createTask(title, todolistId)
+            .then((res) => {
+                setState(res.data)
+            })
+    }, [])
+    return <div>{JSON.stringify(state)}</div>
+}
+
 
 
