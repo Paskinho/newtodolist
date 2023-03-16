9702198941bae21d2853851bc93129b8b88d06f4
Paskinho <v.paskanov@gmail.com>
2023-03-16 19:56:57 +0300

    15 lesson. connect ErrorSnackbar.tsx with state

--- src/components/ErrorSnackbar/ErrorSnackbar.tsx
+++ src/components/ErrorSnackbar/ErrorSnackbar.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import Snackbar from '@mui/material/Snackbar'
 import MuiAlert, { AlertProps } from '@mui/material/Alert'
+import {useAppDispatch, useAppSelector} from "../../app/store";
+import {setErrorAC} from "../../app/app-reducer";
 
 const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
     props, ref) {
@@ -8,16 +10,19 @@ const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
 })
 
 export function CustomizedSnackbars() {
-    const [open, setOpen] = useState(true)
+    const dispatch = useAppDispatch()
+    const error = useAppSelector<null | string>((state) => state.app.error)
 
     const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
         if (reason === 'clickaway') {
             return
         }
-        setOpen(false)
+        dispatch(setErrorAC(null))
+
+
     }
     return (
-        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Snackbar open={!!error} autoHideDuration={6000} onClose={handleClose}>
             <Alert onClose={handleClose} severity='error' sx={{width: '100%'}}>
                 Error message 😠
             </Alert>
