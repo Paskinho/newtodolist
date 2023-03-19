3c0ef4e888dcd4b47bec62927e95b07633f3a9bf
Paskinho <v.paskanov@gmail.com>
2023-03-19 18:50:03 +0300

    15 lesson. Create generic function

--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -1,7 +1,6 @@
 import {AppActionsType, setErrorAC, setStatusAC} from "../app/app-reducer";
 import {Dispatch} from "redux";
 
-
 export const handleServerNetworkError = (dispatch: ErrorUtilsDispatchType, error: {message: string}) => {
     dispatch(setStatusAC('failed'))
     dispatch(setErrorAC(error.message))
