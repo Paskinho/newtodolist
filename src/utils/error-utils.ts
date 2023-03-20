89d1ae9dbc031e714e67a4d795167c7459fee93c
Paskinho <v.paskanov@gmail.com>
2023-03-20 18:19:21 +0300

    15 lesson. Create handleServerAppError with generic

--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -1,9 +1,20 @@
 import {AppActionsType, setErrorAC, setStatusAC} from "../app/app-reducer";
 import {Dispatch} from "redux";
+import {ResponseType} from "../api/todolists-api";
 
 export const handleServerNetworkError = (dispatch: ErrorUtilsDispatchType, error: {message: string}) => {
     dispatch(setStatusAC('failed'))
     dispatch(setErrorAC(error.message))
 }
+
+export const handleServerAppError = <T> (data: ResponseType<T>, dispatch: ErrorUtilsDispatchType) => {
+    if (data.messages.length) {
+        dispatch(setErrorAC(data.messages[0]))
+    } else {
+        dispatch(setErrorAC('Some error'))
+    }
+    dispatch(setStatusAC('idle'))
+}
+
 
 type ErrorUtilsDispatchType = Dispatch<AppActionsType>
