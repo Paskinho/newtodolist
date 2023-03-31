ed463dff7f492e5f3d47437ef0a9c73c66865cfc
Paskinho <v.paskanov@gmail.com>
2023-03-31 19:11:47 +0300

    17 lesson. Create reducers for slice in app-reducer.ts

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -18,8 +18,14 @@ const slice = createSlice({
     name: 'app',
     initialState,
     reducers: {
-        setAppError: (state, action: PayloadAction<{ error: null }>) => {
+        setAppError: (state, action: PayloadAction<{ error: string | null }>) => {
             state.error = action.payload.error
+        },
+        setAppStatus: (state, action: PayloadAction<{ status: RequestStatusType }>)=> {
+            state.status = action.payload.status
+        },
+        setAppInitialized: (state, action: PayloadAction<{ isInitialized: boolean }>) => {
+            state.isInitialized = action.payload.isInitialized
         }
     }
 })
