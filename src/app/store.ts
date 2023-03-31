ad4df124f1be0e30fa5c3dfb1282a040e1a41f4e
Paskinho <v.paskanov@gmail.com>
2023-03-31 18:36:59 +0300

    17 lesson. Create slice in auth-reducer.ts

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,10 @@
 import {tasksReducer} from 'features/TodolistsList/tasks-reducer';
 import {todolistsReducer} from 'features/TodolistsList/todolists-reducer';
-import { AnyAction, applyMiddleware, combineReducers, createStore } from 'redux'
-import thunkMiddleware, { ThunkAction, ThunkDispatch } from 'redux-thunk'
+import {AnyAction, combineReducers} from 'redux'
+import thunk, {ThunkAction, ThunkDispatch} from 'redux-thunk'
 import {appReducer} from './app-reducer'
 import {authReducer} from 'features/Login/auth-reducer';
-import { configureStore } from '@reduxjs/toolkit'
+import {configureStore} from '@reduxjs/toolkit'
 
 
 const rootReducer = combineReducers({
@@ -16,7 +16,8 @@ const rootReducer = combineReducers({
 
 
 export const store = configureStore ({
-    reducer: rootReducer
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunk)
 })
 
 
