89b6f5df2433e4e1930d929891a9e0991ae4cb8c
Paskinho <v.paskanov@gmail.com>
2023-03-31 18:26:33 +0300

    17 lesson. Create store with redux toolkit

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,28 +1,32 @@
-import {tasksReducer} from '../features/TodolistsList/tasks-reducer';
-import {todolistsReducer} from '../features/TodolistsList/todolists-reducer';
+import {tasksReducer} from 'features/TodolistsList/tasks-reducer';
+import {todolistsReducer} from 'features/TodolistsList/todolists-reducer';
 import { AnyAction, applyMiddleware, combineReducers, createStore } from 'redux'
 import thunkMiddleware, { ThunkAction, ThunkDispatch } from 'redux-thunk'
 import {appReducer} from './app-reducer'
-import {authReducer} from '../features/Login/auth-reducer'
+import {authReducer} from 'features/Login/auth-reducer';
+import { configureStore } from '@reduxjs/toolkit'
 
-// объединяя reducer-ы с помощью combineReducers,
-// мы задаём структуру нашего единственного объекта-состояния
+
 const rootReducer = combineReducers({
     tasks: tasksReducer,
     todolists: todolistsReducer,
     app: appReducer,
     auth: authReducer
 })
-// непосредственно создаём store
-export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
-// определить автоматически тип всего объекта состояния
-export type AppRootStateType = ReturnType<typeof rootReducer>
 
+
+export const store = configureStore ({
+    reducer: rootReducer
+})
+
+
+
+
+
+export type AppRootStateType = ReturnType<typeof rootReducer>
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, AnyAction>
 
-// export type AppDispatch = typeof store.dispatch
 export type AppDispatch = ThunkDispatch<AppRootStateType, unknown, AnyAction>
-
 
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
--- src/features/TodolistsList/Todolist/Task/Task.tsx
+++ src/features/TodolistsList/Todolist/Task/Task.tsx
@@ -1,8 +1,8 @@
 import React, { ChangeEvent, useCallback } from 'react'
 import { Checkbox, IconButton } from '@mui/material'
-import { EditableSpan } from '../../../../components/EditableSpan/EditableSpan'
+import { EditableSpan } from 'components/EditableSpan/EditableSpan'
 import { Delete } from '@mui/icons-material'
-import { TaskStatuses, TaskType } from '../../../../api/todolists-api'
+import { TaskStatuses, TaskType } from 'api/todolists-api'
 
 type TaskPropsType = {
 	task: TaskType
