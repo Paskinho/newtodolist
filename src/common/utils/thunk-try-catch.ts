22a285dc1d851817a241cc83e21cb534b0ee28af
Paskinho <v.paskanov@gmail.com>
2023-04-14 20:19:15 +0300

    19 lesson. Create thunk-try-catch.ts in utils

---
+++ src/common/utils/thunk-try-catch.ts
@@ -0,0 +1,18 @@
+import { AppDispatch, AppRootStateType } from 'app/store';
+import { handleServerNetworkError } from 'common/utils/handle-server-network-error';
+import {BaseThunkAPI} from "@reduxjs/toolkit/dist/createAsyncThunk";
+import {appActions} from "app/app.reducer";
+
+export const thunkTryCatch = async (thunkAPI: BaseThunkAPI<AppRootStateType, any, AppDispatch, null>, logic: Function) => {
+    const {dispatch, rejectWithValue} = thunkAPI
+    dispatch(appActions.setAppStatus({status: 'loading'}))
+    try {
+        return await logic()
+    } catch (e) {
+        handleServerNetworkError(e, dispatch)
+        return rejectWithValue(null)
+    } finally {
+        // в handleServerNetworkError можно удалить убирани крутилки
+        dispatch(appActions.setAppStatus({status: 'idle'}))
+    }
+}
